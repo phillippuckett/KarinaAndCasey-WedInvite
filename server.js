05bf3bfa1d62acf3a65630b225e44eecdb4b37af
Phillip Puckett <phillippuckett88@gmail.com>
2016-07-28 14:52:17 -0600

    minor backend changes

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 var express = require('express');
-var bodyparser = require('body-parser');
-var request = require('request');
-
 var app = express();
-
-app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.status(200).end();
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
