1e1d51242cc5314460f6b03c9787fd85d2af19f0
Phillip Puckett <phillippuckett88@gmail.com>
2016-07-28 02:20:21 -0600

    request, body-parser

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.status(200).end();
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Running on Port :', app.get('port'));
+});
