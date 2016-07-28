9747735e749065831c59ed58f72c4627f8b781c4
Phillip Puckett <phillippuckett88@gmail.com>
2016-07-27 18:25:26 -0600

    mobile MVP finished

--- js/controllers.js
+++
@@ -1,7 +0,0 @@
-kcWedding.controller('homeCtrl', function ($scope, $state) { });
-
-kcWedding.controller('aboutCtrl', function ($scope, $state) { });
-
-kcWedding.controller('photoCtrl', function ($scope, $state) { });
-
-kcWedding.controller('registryCtrl', function ($scope, $state) { });
