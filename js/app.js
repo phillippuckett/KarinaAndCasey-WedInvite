06bc8fb1ff7789523bac0471663ad60246a4aec9
Phillip Puckett <phillippuckett88@gmail.com>
2016-07-31 05:16:49 -0600

    styling

--- js/app.js
+++ js/app.js
@@ -18,15 +18,3 @@ window.onclick = function (event) {
   }
 };
 // DROPDOWN MENU //
-
-// META VIEWPORT SCALABILITY //
-function updateWidth() {
-  viewport = document.querySelector("meta[name=viewport]");
-  if (window.orientation == 90 || window.orientation == -90) {
-    viewport.setAttribute('content', 'width=1401, initial-scale=0.34, maximum-scale=1.0, user-scalable=1');
-  }
-  else {
-    viewport.setAttribute('content', 'width=640, initial-scale=0.47, maximum-scale=1.0, user-scalable=1');
-  }
-}
-// META VIEWPORT SCALABILITY //
