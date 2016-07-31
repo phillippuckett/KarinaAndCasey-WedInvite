fefb53606ca5162a5db96fa1c3de57df8adcb0e5
Phillip Puckett <phillippuckett88@gmail.com>
2016-07-31 05:14:21 -0600

    styling

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 // DROPDOWN MENU //
 var dropDown = function () {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.fa-bars')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -18,3 +18,15 @@ window.onclick = function(event) {
   }
 };
 // DROPDOWN MENU //
+
+// META VIEWPORT SCALABILITY //
+function updateWidth() {
+  viewport = document.querySelector("meta[name=viewport]");
+  if (window.orientation == 90 || window.orientation == -90) {
+    viewport.setAttribute('content', 'width=1401, initial-scale=0.34, maximum-scale=1.0, user-scalable=1');
+  }
+  else {
+    viewport.setAttribute('content', 'width=640, initial-scale=0.47, maximum-scale=1.0, user-scalable=1');
+  }
+}
+// META VIEWPORT SCALABILITY //
